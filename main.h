930a13718b9a80b48692bd7ed8fee02ac6465948
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-11-28 03:57:49 -0800

    print_char

--- main.h
+++ main.h
@@ -1,4 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdlib.h>
+#include <stdarg.h>
+int _putchar(char c);
 int _printf(const char *format, ...);
+int print_char(unsigned int z, char c);
+int print_string(unsigned int z, char *s);
 #endif
