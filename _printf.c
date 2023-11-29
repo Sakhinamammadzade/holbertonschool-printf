5fe6918dd9e0fcb2290ed1f6b4f01a4bc3caf3bb
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-11-29 04:53:55 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -13,12 +13,14 @@ int _printf(const char *format, ...)
 	va_list ptr;
 	unsigned int j = 0, r = 0;
 	va_start(ptr, format);
-	for ( j = 0; *(format + j) != '\0'; j++)
+	for (j = 0; *(format + j) != '\0'; j++)
 	{
 		if (*(format + j) != '%')
 		{
 			r++, _putchar(*(format + j));
 		}
+		else if (*(format + j) == '%' && *(format + j + 1) == '%')
+			continue;
 		else if (*(format + j) == '%' && *(format + j + 1) == 'c')
 		{
 			r = print_char(r, va_arg(ptr, int));
