a13c85f8a0abdf7b683b67e9f31f2f2b56809cfc
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-11-29 06:11:35 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -2,36 +2,49 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include "main.h"
+/**
+ * print_string - something usefull
+ * @s: char
+ * @z: int
+ * Return: letter count
+ */
 int print_string(unsigned int z, char *s)
 {
-        int i = 0;
-        if (!s)
-        {
-                putchar('(');
-                putchar('n');
-                putchar('u');
-                putchar('l');
-                putchar('l');
-                putchar(')');
-                z = z + 6;
-        }
-        else
-        {
-                while (*(s + i) != '\0')
-                {
-                        putchar(*(s + i));
-                        i++;
-                        z++;
-                }
-        }
-    return (z);
+	int i = 0;
+
+	if (!s)
+	{
+		_putchar('(');
+		_putchar('n');
+		_putchar('u');
+		_putchar('l');
+		_putchar('l');
+		_putchar(')');
+		z = z + 6;
+	}
+	else
+	{
+		while (*(s + i) != '\0')
+		{
+			_putchar(*(s + i));
+			i++;
+			z++;
+		}
+	}
+	return (z);
 }
+/**
+ * print_char - something usefull
+ * @c: char
+ * @z: int
+ * Return: letter count
+ */
 int print_char(unsigned int z, char c)
 {
-        putchar(c);
-        z++;
-        return (z);
+	_putchar(c);
+	z++;
+	return (z);
 }
 /**
  * _printf - print string
@@ -40,38 +53,40 @@ int print_char(unsigned int z, char c)
  */
 int _printf(const char *format, ...)
 {
-        va_list ptr;
-        unsigned int j = 0, r = 0;
-        if (!format)
-        {
-            exit(98);
-        }
+	va_list ptr;
+
+	unsigned int j = 0, r = 0;
+
+	if (!format)
+	{
+		exit(98);
+	}
 	if (*format == '%' && *(format + 1) == 0)
 		exit(98);
-        va_start(ptr, format);
-        while (*(format + j))
-        {
-                if (*(format + j) == '%' && *(format + j + 1) == '%')
-                {
-                        putchar('%');
-                        r++;
-                        j += 2;
-                }
-                else if (*(format + j) == '%' && *(format + j + 1) == 'c')
-                {
-                        r = print_char(r, (char)va_arg(ptr, int));
-                        j += 2;
-                }
-                else if (*(format + j) == '%' && *(format + j + 1) == 's')
-                {
-                        r = print_string(r, va_arg(ptr, char *));
-                        j += 2;
+	va_start(ptr, format);
+	while (*(format + j))
+	{
+		if (*(format + j) == '%' && *(format + j + 1) == '%')
+		{
+			_putchar('%');
+			r++;
+			j += 2;
 		}
-                else
-                {
-                        r++, putchar(*(format + j));
-                        j++;
-                }
-        }
-        return (r);
+		else if (*(format + j) == '%' && *(format + j + 1) == 'c')
+		{
+			r = print_char(r, (char)va_arg(ptr, int));
+			j += 2;
+		}
+		else if (*(format + j) == '%' && *(format + j + 1) == 's')
+		{
+			r = print_string(r, va_arg(ptr, char *));
+			j += 2;
+		}
+		else
+		{
+			r++, _putchar(*(format + j));
+			j++;
+		}
+	}
+	return (r);
 }
